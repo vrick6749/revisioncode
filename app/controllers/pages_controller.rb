17042d33975db29d1ce44cca9d926d4c92d36469
rick <vrick6749@gmail.com>
2020-04-23 12:57:23 +1000

    testing render to prevent error message, despite expecting 204

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
 
     def index 
+        
+    end
 
+    def show
+        render(plain: "Hello")
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/page", to: "pages#index", as: "homepage"
+
+  get "/page/:id", to: "pages#show"
 end

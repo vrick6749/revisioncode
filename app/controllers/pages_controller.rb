3cc7996ceedb3a0ca12c642914ac1cabbadc389a
rick <vrick6749@gmail.com>
2020-04-23 14:04:53 +1000

    learned rendering html..syntax render(html:textherewithquotationmarks.html_safe)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,8 @@ class PagesController < ApplicationController
         # render(plain: "Hi")   #allows to show code without view page
         puts params[:name] #againt puts will print this param in console
 
-        render(plain: params[:name]) #YOU CAN ONLY RENDER ONCE!!!
+        # render(plain: params[:name]) #YOU CAN ONLY RENDER ONCE!!!
+
+        render(html: "<h1>Hello World</h1>".html_safe) #renders in html format and  html safe ensure no malicious software is in the code
     end
 end
